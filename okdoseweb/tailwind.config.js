461785cbc8c817e9beb3f656a90890984b0ceb33
LauraVilladakommit <laura@kommit.co>
2023-01-23 10:52:34 -0500

    Define the custom color example

--- okdoseweb/tailwind.config.js
+++ okdoseweb/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customColor: '#3f3cbb',
+        customColor: '#3f3cbb', //This is an example a custom color
       },
     },
   },
