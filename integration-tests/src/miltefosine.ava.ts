395db08b5b2910cea2d36c4123694a2fb7720615
LauraVilladakommit <laura@kommit.co>
2023-01-30 12:06:59 -0500

    Refactor integration tests

--- integration-tests/src/miltefosine.ava.ts
+++ integration-tests/src/miltefosine.ava.ts
@@ -1,45 +1,19 @@
-import { NearAccount, Worker } from 'near-workspaces';
-import anyTest, { TestFn } from 'ava';
+import  initContract  from './helpers/initContract';
+import { test } from './helpers/initTest';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
 dotenv.config({path: join(__dirname, '../../', '.env')})
 
-const CONTRACT_ACCOUNT = process.env.MILTEFOSINE_CONTRACT;
+const CONTRACT_ACCOUNT = process.env.MILTEFOSINE_CONTRACT as string;
+const BLOCK_ID = process.env.MILTEFOSINE_BLOCK_ID as string;
+const CONTRACT_METHOD = process.env.CONTRACT_METHOD as string;
 
-const BLOCK_ID = process.env.MILTEFOSINE_BLOCK_ID;
 
-const test = anyTest as TestFn<{
-  worker: Worker;
-  accounts: Record<string, NearAccount>;
-}>;
-
-test.beforeEach(async (t) => {
-  // Init the worker and start a Sandbox server
-  const worker = await Worker.init();
-
-  // Deploy contract
-  const root = worker.rootAccount;
-
-  const contract = await root.importContract({
-    testnetContract: CONTRACT_ACCOUNT,
-    blockId: BLOCK_ID,
-    withData: false,
-  });
-
-  // Save state for test runs, it is unique for each test
-  t.context.worker = worker;
-  t.context.accounts = {root, contract };
-});
-
-test.afterEach.always(async (t) => {
-  await t.context.worker.tearDown().catch(error => {
-    console.log('Failed to tear down the worker:', error);
-  });
-});
+initContract(CONTRACT_ACCOUNT, BLOCK_ID)
 
 test('It returns the correct type attributes', async (t) => {
   const { contract } = t.context.accounts;
-  const dose: any = await contract.view('get_dose', { weight: 60 });
+  const dose: any = await contract.view(CONTRACT_METHOD, { weight: 70 });
 
   t.is(typeof dose.name, 'string');
   t.is(typeof dose.presentation, 'string');
